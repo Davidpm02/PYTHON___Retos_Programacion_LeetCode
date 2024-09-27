568569ed73e6c861d3797e677210cb785776397b
Davidpm02 <davidpamu02@gmail.com>
2024-09-28 00:11:05 +0200

    Nuevo reto de dificultad EASY

---
+++ EASY/Power of Three [326]/challenge.py
@@ -0,0 +1,65 @@
+"""
+DESCRIPTION:
+
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+
+Example 1:
+
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+
+ 
+
+Constraints:
+
+    -231 <= n <= 231 - 1
+
+"""
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+
+        """
+        Summary:
+            Método de la clase Solution encargado de validar si un
+            entero dado es potencia en base 3.
+        Args:
+            n (int) -- Entero a validar como potencia en base 3.
+        Returns:
+            bool
+        """
+        
+        try:
+            assert (n > 0)
+            if n == 1:
+                return True
+                
+            while True:
+                if n == 3:
+                    return True
+                if n < 3:
+                    raise AssertionError
+                
+                n = n / 3
+                continue 
+        except AssertionError:
+            return False
+        
+        return False
