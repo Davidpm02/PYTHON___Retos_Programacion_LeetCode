ee584ef1071c879be7b7d386bcab1b781504dd03
Davidpm02 <davidpamu02@gmail.com>
2024-10-01 18:20:16 +0200

    Nuevo reto de dificultad EASY

--- EASY/Power of Four [342]/challenge.py
+++ EASY/Power of Four [342]/challenge.py
@@ -34,4 +34,25 @@ Follow up: Could you solve it without loops/recursion?
 
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        pass
+        
+        """
+        Summary:
+            Método de la clase Solution encargado de validar un entero
+            como potencia en base 4.
+        Args:
+            n (int) -- Entero a validar como potencia en base 4.
+        Returns:
+            bool
+        """
+
+        while True:
+            try:
+                if n == 1 or n == 4:
+                    return True
+                elif n < 4:
+                    raise AssertionError
+                n = n / 4
+        
+            except AssertionError as e:
+                return False
+        return False
