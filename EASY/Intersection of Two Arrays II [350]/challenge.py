b5d3ff4acafddf404b387fa72c5916e4ef5b3d96
Davidpm02 <davidpamu02@gmail.com>
2024-10-04 17:33:56 +0200

    Nuevo reto de dificultad EASY

--- EASY/Intersection of Two Arrays II [350]/challenge.py
+++ EASY/Intersection of Two Arrays II [350]/challenge.py
@@ -24,3 +24,45 @@ Constraints:
     0 <= nums1[i], nums2[i] <= 1000
 
 """
+
+from collections import Counter
+from typing import List
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        """
+        Summary:
+            Método de la clase Solution encargado de hallar la
+            intersección de dos arrays.
+            Una vez se haya esta intersección, el método la 
+            retorna en forma de lista.
+        Args:
+            nums1 (List[int]) -- Primer array.
+            nums2 (List[int]) -- Segundo array.
+        Returns:
+            List[int]
+        """
+
+        # Defino un contador con las repeticiones de cada entero
+        # en ambos parámetros
+        counter = Counter(nums1)
+        counter.update(nums2)
+
+        # Contadores independientes    
+        counter_nums1 = Counter(nums1)
+        counter_nums2 = Counter(nums2)
+
+        result = []
+        for key, value in counter.items():
+
+            times_in_array1 = counter_nums1[key]
+            times_in_array2 = counter_nums2[key]
+
+            if (times_in_array1 == times_in_array2):
+                for _ in range(times_in_array1):
+                    result.append(key)
+            else:
+                min_reps_in_nums = min([times_in_array1, times_in_array2])
+                for _ in range(min_reps_in_nums):
+                    result.append(key)
+        return result
