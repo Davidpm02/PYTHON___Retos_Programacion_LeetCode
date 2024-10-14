724ed124f5137ab488150f90bc62636b7d252956
Davidpm02 <davidpamu02@gmail.com>
2024-10-14 18:19:59 +0200

    Nuevo reto de dificultad EASY

--- EASY/Is Subsequence [392]/challenge.py
+++ EASY/Is Subsequence [392]/challenge.py
@@ -27,4 +27,27 @@ Constraints:
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pass
+        
+        """
+        Summary:
+            Método de la clase Solution encargado de verificar que
+            una cadena representa una subsecuencia de una cadena original.
+        Args:
+            s (str) -- Cadena original.
+            t (str) -- Subsecuencia a validar por el método.
+        Returns:
+            bool
+
+        """
+
+        chars_in_s = [char for char in t if char in s]
+        for idx, char in enumerate(chars_in_s):
+            try:
+                if chars_in_s[idx] != s[idx]:
+                    chars_in_s.remove(char)
+                    assert chars_in_s[idx + 1] == s[idx + 1]
+            except AssertionError:
+                continue
+            except Exception:
+                break
+        return True if ("".join(chars_in_s) == s) else False
