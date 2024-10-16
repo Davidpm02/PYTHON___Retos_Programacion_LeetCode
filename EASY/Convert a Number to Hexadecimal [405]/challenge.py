51fbfaf00a7471a0bd0cc9f897914084030dfb86
Davidpm02 <davidpamu02@gmail.com>
2024-10-16 18:52:14 +0200

    Docstring + planteamiento inicial

--- EASY/Convert a Number to Hexadecimal [405]/challenge.py
+++ EASY/Convert a Number to Hexadecimal [405]/challenge.py
@@ -27,4 +27,49 @@ Constraints:
 
 class Solution:
     def toHex(self, num: int) -> str:
-        pass
+        
+        """
+        Summary:
+            Método de la clase Solution encargado de retornar una
+            cadena que represente un entero dado en 32-bits en formato
+            hexadecimal.
+        Args:
+            num (int) -- Entero de 32-bits.
+        Returns:
+            str
+        """
+
+        # Arrays de referencia para conversión decimal-hexadecimal
+        decimals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+                    11, 12, 13, 14, 15, 16, 17, 18, 19,
+                    20, 21, 22, 23, 24, 25, 26, 27, 28,
+                    29, 30, 40, 50, 60, 70, 80, 90, 100,
+                    200, 1000, 2000]
+
+        hexadecimals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                        'A', 'B', 'C', 'D', 'E', 'F', '10', '11', '12', '13',
+                        '14', '15', '16', '17', '18', '19', '1A', '1B', '1C',
+                        '1D', '1E', '28', '32', '3C', '46', '50', '5A', '64',
+                        'C8', '3E8', '7D0']
+
+        # Último cociente
+        last_quotient = None
+
+        # Restos
+        remainders = []
+
+        while True:
+            if last_quotient == 0:
+                break
+            last_quotient = num // 16
+            remainders.append((num % 16))
+
+            num = last_quotient
+        
+        # Construyendo la representación hexadecimal de 'num'
+        hexadecimal_repr_content = []
+        for r in remainders[::-1]:
+            index_in_decimals_array = decimals.index(r)
+            hexadecimal_repr_content.append(hexadecimals[index_in_decimals_array])
+
+        return "".join(hexadecimal_repr_content).lower()
