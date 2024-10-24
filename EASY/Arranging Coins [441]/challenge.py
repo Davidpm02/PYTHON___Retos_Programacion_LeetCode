39e4e6d7e531016a06d11854c40fde271546d249
Davidpm02 <davidpamu02@gmail.com>
2024-10-24 18:15:07 +0200

    Nuevo reto de dificultad EASY (necesaria UPDATE para mejorar EFICIENCIA)

--- EASY/Arranging Coins [441]/challenge.py
+++ EASY/Arranging Coins [441]/challenge.py
@@ -29,4 +29,36 @@ Constraints:
 
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        pass
+        
+        """
+        Summary:
+            Método de la clase Solution encargado de calcular
+            el número de escalones que se completan con 'n'
+            monedas, asumiendo que el último escalón siempre
+            debe estar incompleto.
+        Args:
+            n (int) -- Número de monedas de las que se disponen
+            para construir la escalera.
+
+        Returns:
+            int -- Número de escalones completos con las monedas
+            disponibles.
+        """
+
+        completed_rows = 0
+        coins_in_stairs = []
+        while True:
+            if len(coins_in_stairs) == 0:
+                coins_in_stairs.append(1)
+                completed_rows += 1
+                n -= 1
+            
+            try:
+                # Aseguramos que tenemos suficientes monedas para
+                # completar el siguiente escalon.
+                assert coins_in_stairs[-1] < n
+                coins_in_stairs.append(coins_in_stairs[-1] + 1)
+                completed_rows += 1
+                n -= coins_in_stairs[-1]
+            except AssertionError:
+                return completed_rows
