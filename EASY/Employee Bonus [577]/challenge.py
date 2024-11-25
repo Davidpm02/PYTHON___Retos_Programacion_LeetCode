eed390994bed8d049a67c23858344eb9bfd2da2a
Davidpm02 <davidpamu02@gmail.com>
2024-11-25 18:53:28 +0100

    Nuevo reto de dificultad EASY

--- EASY/Employee Bonus [577]/challenge.py
+++ EASY/Employee Bonus [577]/challenge.py
@@ -69,3 +69,47 @@ Output:
 +------+-------+
 
 """
+
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    
+    """
+    Se encarga de verificar todos los empleados que reciben un bonus inferior a 
+    1000 o no reciben bonus, y retorna un DataFrame con los resultados.
+
+    params:
+        - employee (pd.DataFrame)
+        - bonus (pd.DataFrame)
+    
+    returns:
+        pd.DataFrame -- Dataframe obtenido como resultado.
+    """
+
+    # Filtrar empleados con bonus menor a 1000
+    empId_less_than_1000 = bonus.loc[bonus["bonus"] < 1000, "empId"].values
+
+    # Empleados que no tienen bonus registrados
+    empId_without_bonus = employee.loc[~employee["empId"].isin(bonus["empId"]), "empId"].values
+
+    # Combinar los IDs de empleados sin bonus y con bonus < 1000
+    empId_less_1000_bonus = empId_less_than_1000.tolist()
+    empId_less_1000_bonus.extend(empId_without_bonus)
+
+    # Obtener nombres de empleados con bonus < 1000 o sin bonus
+    names_employees_with_less_1000_bonus = employee.loc[employee["empId"].isin(empId_less_1000_bonus), "name"].values
+    
+    employees_and_bonus = []
+    for empId in empId_less_1000_bonus:
+        if empId in bonus["empId"].values:
+            employee_name = employee.loc[employee["empId"] == empId, "name"].values
+            employee_bonus = bonus.loc[bonus["empId"] == empId, "bonus"].values
+        else:
+            employee_name = employee.loc[employee["empId"] == empId, "name"].values
+            employee_bonus = None
+        
+        employee_info = (employee_name[0], employee_bonus[0] if (employee_bonus is not None) else None)
+        employees_and_bonus.append(employee_info)
+
+    return pd.DataFrame(data=employees_and_bonus,
+                        columns=["name", "bonus"])
