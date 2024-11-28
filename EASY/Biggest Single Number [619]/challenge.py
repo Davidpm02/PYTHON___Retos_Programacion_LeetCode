8d3fe5e940c98a60caee19ea2cef772db03e1af3
Davidpm02 <davidpamu02@gmail.com>
2024-11-28 18:50:18 +0100

    Nuevo reto de dificultad EASY

--- EASY/Biggest Single Number [619]/challenge.py
+++ EASY/Biggest Single Number [619]/challenge.py
@@ -74,3 +74,41 @@ Output:
 Explanation: There are no single numbers in the input table so we return null.
 
 """
+
+import pandas as pd
+from collections import Counter
+
+def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
+
+    """
+    Encuentra el número con una única aparición dentro del 'my_numbers'
+    y lo retorna dentro de un nuevo objeto DataFrame.
+
+    En caso de que no existan números con una única aparición, la función
+    retorna un DataFrame con un único registro nulo.
+
+    params:
+        my_numbers (pd.DataFrame)
+    
+    returns:
+        pd.DataFrame
+    """
+
+    numbers_record = list(my_numbers['num'].values)
+    counter_of_numbers = Counter(numbers_record)
+
+    single_number_list = []
+    for key, value in counter_of_numbers.items():
+        
+        # Cada elemento de Counter tiene la estructura (key:value) --> Diccionario
+        if (value == 1):
+            single_number_list.append(key)
+        else:
+            continue
+    
+    if (len(single_number_list) > 0):
+        highest_single_number = max(single_number_list)
+    else:
+        highest_single_number = None
+    return pd.DataFrame(data=[highest_single_number],
+                        columns=['num'])
