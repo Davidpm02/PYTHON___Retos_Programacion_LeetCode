5c08ff5440a8be9b346d008be3774f93b7f70bbd
Davidpm02 <davidpamu02@gmail.com>
2024-12-10 20:46:17 +0100

    Nuevo reto de dificultad EASY

--- EASY/Degree of an Array [697]/challenge.py
+++ EASY/Degree of an Array [697]/challenge.py
@@ -33,3 +33,37 @@ Constraints:
     nums[i] will be an integer between 0 and 49,999.
 
 """
+
+from collections import Counter
+from typing import List
+
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        
+        """
+        Halla la longitud de la mínima sublista de mayor grado dentro
+        de una lista dada.
+
+        params:
+            nums(List[int])
+        
+        returns:
+            int
+        """
+
+        reps_counter = Counter(nums)
+        ordered_reps_counter = reps_counter.most_common()
+
+        max_degree_in_array = ordered_reps_counter[0][-1]
+
+        # Elementos de mayor grado en 'nums'
+        higher_degree_integers = { item[0]:[] for item in ordered_reps_counter if (item[-1] == max_degree_in_array)}
+        
+        # Recorro el diccionario para rellenar la lista de índices
+        for key in higher_degree_integers.keys():
+            higher_degree_integers[key] = [i for i in range(len(nums)) if nums[i] == key]
+        
+        # Defino una lista con las diferencias de índices para cada
+        # clave del diccionario
+        difference_among_indexes = [(value[-1] - value[0]) for key, value in higher_degree_integers.items()]
+        return min(difference_among_indexes) + 1
