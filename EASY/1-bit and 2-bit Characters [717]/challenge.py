af761d3a5a2d2bef55775bdb776c06219c858928
Davidpm02 <davidpamu02@gmail.com>
2024-12-19 21:04:58 +0100

    Nuevo reto de dificultad EASY

--- EASY/1-bit and 2-bit Characters [717]/challenge.py
+++ EASY/1-bit and 2-bit Characters [717]/challenge.py
@@ -32,3 +32,45 @@ Constraints:
     bits[i] is either 0 or 1.
 
 """
+
+from typing import List
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        
+        """
+        Dada las condiciones del reto, se encarga de verificar
+        si el último carácter de 'bits' debe ser un carácter de
+        un único bit.
+
+        params:
+            bits (List[int])
+        
+        returns:
+            bool
+        """
+
+        original_bits = bits.copy()
+        if (len(bits) == 1):
+            # El único elemento es 0; se retorna True
+            return True
+
+        idx_counter = 0
+        while True:
+            for bit in bits:
+                if (len(bits) == 1):
+                    # El único elemento es 0; se retorna True
+                    return True
+                        
+                if (bit == 0):
+                    idx_counter +=1
+                    bits = original_bits[idx_counter:]
+                    break
+                else:
+                    idx_counter +=2
+                    bits = original_bits[idx_counter:]
+                    break
+            if (len(bits) > 0):
+                continue
+            else:
+                return False
