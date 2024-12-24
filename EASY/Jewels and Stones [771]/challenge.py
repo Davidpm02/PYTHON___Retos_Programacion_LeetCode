083d5f92a92ba13d0be96d8cdc12f172ca9b965b
Davidpm02 <davidpamu02@gmail.com>
2024-12-24 02:19:55 +0100

    Nuevo reto de dificultad EASY

--- EASY/Jewels and Stones [771]/challenge.py
+++ EASY/Jewels and Stones [771]/challenge.py
@@ -27,3 +27,28 @@ Constraints:
 
 """
 
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        
+        """
+        Retorna el total de joyas que se tienen con respecto a un
+        inventario de piedras dado.
+
+        params:
+            jewels (str) -- Joyas de referencia a examinar.
+            stones (str) -- Inventario de piedras disponible.
+        
+        returns:
+            int -- Total de joyas dentro del inventario de piedras
+            disponible.
+        """
+
+        # Lista con las joyas que tenemos de referencia
+        jewels_list = [jewel for jewel in jewels]
+
+        # Contador de joyas con respecto al total de piedras
+        jewels_counter = 0
+        for jewel in jewels_list:
+            if (jewel in stones):
+                jewels_counter += stones.count(jewel)
+        return jewels_counter
