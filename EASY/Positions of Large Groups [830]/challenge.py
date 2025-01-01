d642165263188b3b39f08b7fb053837caeb75c97
Davidpm02 <davidpamu02@gmail.com>
2025-01-01 20:28:56 +0100

    Nuevo reto de dificultad EASY

--- EASY/Positions of Large Groups [830]/challenge.py
+++ EASY/Positions of Large Groups [830]/challenge.py
@@ -39,3 +39,71 @@ Constraints:
     s contains lowercase English letters only.
 
 """
+
+from typing import List
+
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        
+        """
+        Se encarga de obtener los índices que primer y último
+        carácter que contiene cada uno de los grupos de letras
+        contenido en la cadena recibida como parámetro.
+
+        La solución entregada por la función tiene en cuenta
+        únicamente los grupos considerados grandes (3 caracteres
+        o más en el mismo grupo).
+
+        params:
+            s (str)
+        
+        returns:
+            List[List[int]]
+        """
+
+        # Lista con cada carácter en 's'
+        chars_on_string = [char for char in s]
+        starting_indexes_for_groups = []
+
+
+        # Actualizo el contenido de 'starting_indexes_for_groups'
+        for idx, char in enumerate(chars_on_string):
+            if (idx == 0):
+                starting_indexes_for_groups.append(idx)
+                continue
+            else:
+                if (char != chars_on_string[idx - 1]):
+                    starting_indexes_for_groups.append(idx)
+
+
+        # Proceso la lista para formar los grupos de caracteres
+        while True:
+            for idx, char in enumerate(chars_on_string):
+                if (idx == 0):
+                    continue
+                else:
+                    if (char == chars_on_string[idx - 1]):
+                        chars_on_string[idx - 1] += char
+                        chars_on_string.pop(idx)
+                        break
+            else:
+                # Segunda pasada para reafinar los grupos creados
+                for idx, char in enumerate(chars_on_string):
+                    if (idx == 0):
+                        continue
+                    else:
+                        if (char in chars_on_string[idx - 1]):
+                            chars_on_string[idx - 1] += char
+                            chars_on_string.pop(idx)
+                            break
+                else:
+                    break
+        
+        # Lista para almacenar el rango de índices de cada grupo
+        # considerado grande.
+        large_groups_indexes = [
+            [starting_indexes_for_groups[idx], (starting_indexes_for_groups[idx] + (len(group) - 1))]
+            for idx, group in enumerate(chars_on_string)
+            if (len(group) >= 3)
+        ]
+        return large_groups_indexes
