50b89e76a70074017f140e8f9e9b0a6f26729923
Davidpm02 <davidpamu02@gmail.com>
2025-01-01 20:35:12 +0100

    Nuevo reto de dificultad EASY

--- EASY/Buddy Strings [859]/challenge.py
+++ EASY/Buddy Strings [859]/challenge.py
@@ -36,3 +36,60 @@ Constraints:
 
 """
 
+from collections import Counter
+
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        
+        """
+        Se encarga de verificar si dos cadenas de texto recibidas como
+        parámetro se consideran 'buddy strings'.
+
+        Según su definición, dos cadenas reciben esta cualidad si, al
+        intercambiar dos caracteres en una de ellas, ambas mantienen
+        el mismo contenido.
+
+        params:
+            s (str)
+            goal (str)
+        
+        returns:
+            bool
+        """
+
+        # Listas de caracteres diferentes en la cadena principal y sus
+        # índices con respecto a la cadena objetivo.
+        different_chars_in_s = []
+        idxs_of_different_chars_in_s = []
+
+        if (len(s) != len(goal)):
+            return False
+
+        # Actualizo el contador en función del contenido de la cadena.
+        for idx, char in enumerate(s):
+            try:
+                assert (char == goal[idx])
+            except AssertionError:
+                different_chars_in_s.append(char)
+                idxs_of_different_chars_in_s.append(idx)
+        
+        if (len(different_chars_in_s) == 2):
+            # Analizamos si al intercambiar los caracteres, la cadena es
+            # igual a la cadena objetivo.
+            chars_in_s = [char for char in s]
+            chars_in_s[idxs_of_different_chars_in_s[0]] = different_chars_in_s[1]
+            chars_in_s[idxs_of_different_chars_in_s[1]] = different_chars_in_s[0]
+
+            return True if ("".join(chars_in_s) == goal) else False
+        if (len(different_chars_in_s) == 0):
+            # Defino un contador para los caracteres de ambas cadenas
+            chars_in_s_counter = Counter(s)
+            chars_in_goal_counter = Counter(goal)
+
+            # Busco entre los caracteres de las cadenas y reviso si alguno
+            # se repite, al menos, en dos ocasiones
+            for char, reps in chars_in_s_counter.items():
+                if ((reps >= 2) and (chars_in_goal_counter[char] == reps)):
+                    return True
+        return False
+        
