02e80d728125cd0f85ba68cfac94af75cbf68570
Davidpm02 <davidpamu02@gmail.com>
2025-01-01 14:04:40 +0100

    Nuevo reto diario

--- EASY/Most Common Word [819]/challenge.py
+++ EASY/Most Common Word [819]/challenge.py
@@ -35,3 +35,43 @@ Constraints:
 
 """
 
+from collections import Counter
+from typing import List
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        
+        """
+        Se encarga de hallar la palabra más común dentro de una cadena
+        recibida como parámetro. La función tiene en cuenta las palabras
+        del parámetro 'banned' para ofrecer un resultado válido.
+
+        params:
+            paragraph (str)
+            banned (List[str])
+        
+        returns:
+            str
+        """
+
+        # Proceso 'paragraph' para eliminar caracteres no válidos
+        non_valid_chars = [char for char in "!?',;."]
+        for non_valid_char in non_valid_chars:
+            paragraph = paragraph.replace(non_valid_char, "")
+        print(paragraph)
+        # Actualizo el contenido de 'paragraph' a minúsculas
+        paragraph = paragraph.lower()
+
+        # Elimino todos los elementos prohibidos
+        for word in [_.lower() for _ in banned]:
+            if word in [__.lower() for __ in paragraph.split()]:
+                paragraph = paragraph.replace(word, "")
+     
+        print(paragraph)
+
+        # Defino un objeto Contador de todos los elementos en 'paragraph'
+        # y retorno el más común
+        str_counter = Counter([word.lower() for word in paragraph.split()])
+        print(str_counter)
+        return str_counter.most_common()[0][0]
+        
