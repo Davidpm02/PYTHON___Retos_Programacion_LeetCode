60b098bdc89b36fc7d95930c107eb02700dafc6c
Davidpm02 <davidpamu02@gmail.com>
2025-02-10 19:09:38 +0100

    Nuevo reto de dificultad EASY

--- EASY/Clear Digits [3174]/challenge.py
+++ EASY/Clear Digits [3174]/challenge.py
@@ -43,3 +43,34 @@ Constraints:
 
 """
 
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        
+
+        """
+        Se encarga de procesar una cadena de texto y eliminar todos los
+        caracteres que sean dígitos, además del caracter más cercano a
+        este situado a la izquierda.
+
+        params:
+            s (str)
+        
+        returns:
+            str
+        """
+
+        idx_marked = {idx:1 if char in '0123456789' else 0 for idx, char in enumerate(s)}
+
+        while (1 in list(idx_marked.values())):
+            for idx, is_marked in idx_marked.items():
+                if (is_marked == 1):
+                    del idx_marked[idx]
+                    for i in reversed(range(0, idx)):
+                        if (i in idx_marked):
+                            del idx_marked[i]
+                            break
+                        continue
+                    break
+
+        valid_chars = [char for idx, char in enumerate(s) if (idx in idx_marked)]
+        return "".join(valid_chars)
