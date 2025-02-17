f7e36b9a09f91de90cc5a4dba604a32c39d005b0
Davidpm02 <davidpamu02@gmail.com>
2025-02-17 18:57:48 +0100

    Nuevo reto de dificultad MEDIUM

--- MEDIUM/Letter Tile Possibilities [1079]/challenge.py
+++ MEDIUM/Letter Tile Possibilities [1079]/challenge.py
@@ -32,3 +32,33 @@ Constraints:
 
 """
 
+from itertools import permutations
+
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        
+        """
+        Se encarga de hallar el número total de posibilidades que
+        pueden formarse a partir de los caracteres de las baldosas
+        en el parámetro 'tiles'.
+
+        params:
+            tiles (str)
+        
+        returns:
+            int
+        """
+
+        # Conjunto de todas las permutaciones posibles con los
+        # caracteres de la cadena.
+        seen = set()
+
+        # Se itera sobre las longitudes posibles de las permutaciones,
+        # y se añaden todas las combinaciones posibles al conjunto.
+        for i in range(1, len(tiles) + 1):
+            for perm in permutations(tiles, i):
+                seen.add(perm)
+        return len(seen)
+
+        # El uso de permutaciones es una de las mejores estrategias 
+        # posibles si tenemos en cuenta las restricciones del ejercicio.
