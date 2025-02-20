95a1bc6a137e0760bc1506f653813902a7d4fedf
Davidpm02 <davidpamu02@gmail.com>
2025-02-20 18:51:27 +0100

    Nuevo reto de dificultad MEDIUM

--- MEDIUM/Find Unique Binary String [1980]/challenge.py
+++ MEDIUM/Find Unique Binary String [1980]/challenge.py
@@ -34,3 +34,55 @@ Constraints:
     All the strings of nums are unique.
 
 """
+
+from typing import List
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        
+        """
+        Se encarga de hallar una representación binaria cualquiera
+        de longitud 'n' que no aparezca en el parámetro 'nums'.
+
+        Si exiten varias opciones disponibles, la función retornará
+        una cualquiera.
+
+        params:
+            nums (List[str])
+
+        returns:
+            str        
+        """
+
+        # Longitud de cada número en nums
+        length_num = len(nums[0])
+
+        if ((len(nums) == 1) and (len(nums[0]) == 1)):     # Valido la longitud de nums y devuelvo el resultado
+            return "1" if (nums[0] == "0") else "0"        # si se cumplen las condiciones.
+
+        # Verifico primero si se encuentra el 0 dentro del listado
+        # de nums
+        if (("0" * length_num) not in nums):
+            return "0" * length_num
+        else:
+            # Contador de iteraciones completadas
+            fully_iterated = 0
+            while (fully_iterated != length_num):
+                # Inicializo un array con length_num elementos == 0
+                initial_array = ["0"] * length_num
+
+                if (fully_iterated > 0):
+                    for _ in range(0, fully_iterated + 1):
+                        initial_array[_] = "1"
+                    if ("".join(initial_array) not in nums):
+                        return "".join(initial_array)
+
+                # Itero sobre el total de la longitud de cada número
+                for i in range(length_num - 1, 0 , -1):
+                    initial_array[i] = "1"
+                    if ("".join(initial_array) not in nums):
+                        return "".join(initial_array)
+                    else:
+                        initial_array[i] = "0"
+                        continue
+                # Incremento el contador de iteraciones completadas
+                fully_iterated += 1
