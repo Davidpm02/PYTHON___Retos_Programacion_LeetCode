53cd9024d4536284f3912c6281fa24cd88355d81
Davidpm02 <davidpamu02@gmail.com>
2025-02-28 14:19:30 +0100

    Nuevo reto de dificultad HARD

--- HARD/Shortest Common Supersequence [1092]/challenge.py
+++ HARD/Shortest Common Supersequence [1092]/challenge.py
@@ -30,3 +30,57 @@ Constraints:
 
 """
 
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+
+        """
+        Se encarga de hallar la subcadena común más corta posible,
+        a partir de las cadenas 'str1' y 'str2' recibidas como 
+        parámetro.
+
+        params:
+            str1 (str)
+            str2 (str)
+        
+        returns:
+            str
+        """
+
+        # Paso 1: Encontrar la subsecuencia común más larga (LCS)
+        m, n = len(str1), len(str2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # Construimos la matriz de LCS usando programación dinámica
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        # Paso 2: Construir la supersecuencia más corta usando la LCS
+        i, j = m, n
+        scs = []
+
+        while i > 0 and j > 0:
+            if str1[i - 1] == str2[j - 1]:  # Coincidencia en el LCS
+                scs.append(str1[i - 1])
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:  # Nos movemos hacia arriba
+                scs.append(str1[i - 1])
+                i -= 1
+            else:  # Nos movemos hacia la izquierda
+                scs.append(str2[j - 1])
+                j -= 1
+
+        # Agregar caracteres restantes de str1 o str2
+        while i > 0:
+            scs.append(str1[i - 1])
+            i -= 1
+        while j > 0:
+            scs.append(str2[j - 1])
+            j -= 1
+
+        # La respuesta está en orden inverso
+        return ''.join(reversed(scs))
