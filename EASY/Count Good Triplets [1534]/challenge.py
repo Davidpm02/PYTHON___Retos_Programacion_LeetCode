560ad1f617f4d60b3c70bde48f936f4d59539cb9
Davidpm02 <davidpamu02@gmail.com>
2025-04-14 23:03:45 +0200

    Nuevo reto de dificultad EASY

--- EASY/Count Good Triplets [1534]/challenge.py
+++ EASY/Count Good Triplets [1534]/challenge.py
@@ -34,3 +34,35 @@ Constraints:
 0 <= a, b, c <= 1000
 
 """
+
+from typing import List
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+
+        """
+        Se encarga de contar el número de 'buenos tripletes' dentro de un
+        array 'arr', dados los valores 'a', 'b' y 'c'.
+
+        params:
+            arr (List[int])
+            a (int)
+            b (int)
+            c (int)
+
+        returns:
+            int
+        """
+        
+        n = len(arr)
+        count = 0
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                # Verificar la primera condición
+                if abs(arr[i] - arr[j]) <= a:
+                    for k in range(j + 1, n):
+                        # Verificar las otras dos condiciones
+                        if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                            count += 1
+        
+        return count
