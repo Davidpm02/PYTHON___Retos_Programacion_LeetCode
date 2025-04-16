baa91fe7064992b322a6e6608610c972b48eee84
Davidpm02 <davidpamu02@gmail.com>
2025-04-16 19:44:35 +0200

    Nuevo reto de dificultad MEDIUM

--- MEDIUM/Count the Number of Good Subarrays [2537]/challenge.py
+++ MEDIUM/Count the Number of Good Subarrays [2537]/challenge.py
@@ -32,3 +32,46 @@ Constraints:
 
 """
 
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        
+        """
+        Se encarga de contar el número de subarrays dentro de 'nums'
+        que tienen, al menos, 'k' pares de índices (i, j) tales que
+        i < j y arr[i] == arr[j].
+
+        params:
+            nums (List[int])
+            k (int)
+
+        returns:
+            int
+        """
+
+        freq = defaultdict(int)
+        n = len(nums)
+        left = 0  # Límite izquierdo de la ventana
+        total_pairs = 0  # Cantidad de pares dentro de la ventana
+        result = 0  # Acumulador de subarrays buenos
+
+        # Recorro el array con el extremo derecho de la ventana
+        for right in range(n):
+            num = nums[right]
+            # Cada vez que agrego un número, contribuye con freq[num] pares adicionales
+            total_pairs += freq[num]
+            freq[num] += 1
+
+            # Mientras la ventana sea válida (pares >= k), intento acortarla por la izquierda
+            while total_pairs >= k:
+                # Como tengo al menos k pares, todos los subarrays desde left hasta el final son válidos
+                result += n - right
+
+                # Quito nums[left] de la ventana, ajustando los pares
+                freq[nums[left]] -= 1
+                total_pairs -= freq[nums[left]]
+                left += 1
+
+        return result
