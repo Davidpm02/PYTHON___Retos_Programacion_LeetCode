cbb7388720ea898ba9ba186cf6b2114b610578e6
Davidpm02 <davidpamu02@gmail.com>
2025-06-07 22:39:35 +0200

    Nuevo reto de dificultad MEDIUM

--- MEDIUM/Lexicographically Minimum String After Removing Stars [3170]/challenge.py
+++ MEDIUM/Lexicographically Minimum String After Removing Stars [3170]/challenge.py
@@ -40,3 +40,33 @@ The input is generated such that it is possible to delete all '*' characters.
 
 """
 
+class Solution:
+    def clearStars(self, s: str) -> str:
+        """
+        Elimina todos los '*' de la cadena s, borrando para cada '*' el carácter más pequeño a su izquierda.
+        """
+        # Inicializo estructuras
+        result = []  
+        deleted = []
+
+        # Listas de posiciones para cada letra
+        positions = [[] for _ in range(26)]
+
+        for ch in s:
+            if ch != '*':
+                # Añado el carácter y lo marco como no eliminado
+                result.append(ch)
+                deleted.append(False)
+                # Registro su posición
+                idx = len(result) - 1
+                positions[ord(ch) - ord('a')].append(idx)
+            else:
+                # Elimino el carácter más pequeño disponible
+                for i in range(26):
+                    if positions[i]:
+                        pos = positions[i].pop()  # Cojo la última (más cercana al '*')
+                        deleted[pos] = True      # Marco como eliminado
+                        break
+        
+        # Construyo la cadena final
+        return ''.join(ch for ch, del_flag in zip(result, deleted) if not del_flag)
