118aa9a5841839e4fb52abd7d4a8d6c8014c2308
Davidpm02 <davidpamu02@gmail.com>
2025-06-29 11:34:09 +0200

    Nuevo reto de dificultad MEDIUM

--- MEDIUM/Number of Subsequences That Satisfy the Given Sum Condition [1498]/challenge.py
+++ MEDIUM/Number of Subsequences That Satisfy the Given Sum Condition [1498]/challenge.py
@@ -38,3 +38,31 @@ Constraints:
 
 """
 
+from typing import List
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        
+        MOD = 10**9 + 7
+        nums.sort()
+        n = len(nums)
+        
+        # Precalculo potencias de 2 hasta n
+        pow2 = [1] * n
+        for i in range(1, n):
+            pow2[i] = (pow2[i - 1] * 2) % MOD
+        
+        res = 0
+        left, right = 0, n - 1
+        
+        while left <= right:
+            # Si la suma del menor y el mayor es válida
+            if nums[left] + nums[right] <= target:
+                # Puedo formar 2^(right - left) subsecuencias válidas
+                res = (res + pow2[right - left]) % MOD
+                left += 1
+            else:
+                # Si no cumple, reduzco el mayor
+                right -= 1
+        
+        return res
